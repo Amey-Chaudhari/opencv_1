5cbf4a33574fcc5b534984cfe7d567f06b9a1dea
Amey-Chaudhari <64803502+Amey-Chaudhari@users.noreply.github.com>
2021-12-09 23:56:44 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -13,3 +13,6 @@ arr1=np.random.randint(5,size=(2,3))#random integer no.
 print(arr1)
 arr1=np.random.rand(2,3)*255 #scalar matrix multiplication
 print(arr1)
+print(arr1.shape) #height,width
+print(arr1.size) #no. of elements
+print(arr.min)
